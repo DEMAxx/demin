91ecd72335eae756c3863d821f8ce03ed5331849
Stas Demin <demin.sv@cdek.ru>
2024-12-09 15:33:16 +0300

    first commit

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var b strings.Builder
+	saveValue := ""
+
+	for _, v := range s {
+		a, err := strconv.Atoi(string(v))
+
+		if err == nil {
+			if saveValue == "\\" {
+				saveValue = strconv.Itoa(a)
+				continue
+			} else {
+				if saveValue == "" {
+					return "", ErrInvalidString
+				}
+				fmt.Fprintf(&b, "%s", strings.Repeat(saveValue, a))
+			}
+
+			saveValue = ""
+		} else {
+			if saveValue == "\\" && string(v) == "\\" {
+				fmt.Fprintf(&b, "%s", saveValue)
+				saveValue = ""
+			}
+
+			if saveValue != "" {
+				fmt.Fprintf(&b, "%s", saveValue)
+			}
+
+			saveValue = string(v)
+		}
+	}
+
+	if saveValue != "" {
+		fmt.Fprintf(&b, "%s", saveValue)
+	}
+
+	return b.String(), nil
 }
