e8b734f3c43b6806f4b2ae3027ab04a67e4007be
Stas Demin <demin.sv@cdek.ru>
2024-12-09 16:10:39 +0300

    all tests done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,6 +12,7 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(s string) (string, error) {
 	var b strings.Builder
 	saveValue := ""
+	duplicateSlash := false
 
 	for _, v := range s {
 		a, err := strconv.Atoi(string(v))
@@ -22,16 +23,24 @@ func Unpack(s string) (string, error) {
 				continue
 			} else {
 				if saveValue == "" {
-					return "", ErrInvalidString
+					if duplicateSlash {
+						saveValue = "\\"
+						a = a - 1
+					} else {
+						return "", ErrInvalidString
+					}
 				}
 				fmt.Fprintf(&b, "%s", strings.Repeat(saveValue, a))
 			}
 
+			duplicateSlash = false
 			saveValue = ""
 		} else {
 			if saveValue == "\\" && string(v) == "\\" {
 				fmt.Fprintf(&b, "%s", saveValue)
+				duplicateSlash = true
 				saveValue = ""
+				continue
 			}
 
 			if saveValue != "" {
