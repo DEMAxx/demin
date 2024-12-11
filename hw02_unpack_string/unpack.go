21eabd786b89a3cc2ded978c6dada8b36488f967
Stas Demin <demin.sv@cdek.ru>
2024-12-11 09:28:13 +0300

    linter fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -17,38 +17,40 @@ func Unpack(s string) (string, error) {
 	for _, v := range s {
 		a, err := strconv.Atoi(string(v))
 
+		if err == nil && saveValue == "\\" {
+			saveValue = strconv.Itoa(a)
+			continue
+		}
+
 		if err == nil {
-			if saveValue == "\\" {
-				saveValue = strconv.Itoa(a)
-				continue
-			} else {
-				if saveValue == "" {
-					if duplicateSlash {
-						saveValue = "\\"
-						a = a - 1
-					} else {
-						return "", ErrInvalidString
-					}
+			if saveValue == "" {
+				if duplicateSlash {
+					saveValue = "\\"
+					a--
+				} else {
+					return "", ErrInvalidString
 				}
-				fmt.Fprintf(&b, "%s", strings.Repeat(saveValue, a))
 			}
+
+			fmt.Fprintf(&b, "%s", strings.Repeat(saveValue, a))
 
 			duplicateSlash = false
 			saveValue = ""
-		} else {
-			if saveValue == "\\" && string(v) == "\\" {
-				fmt.Fprintf(&b, "%s", saveValue)
-				duplicateSlash = true
-				saveValue = ""
-				continue
-			}
-
-			if saveValue != "" {
-				fmt.Fprintf(&b, "%s", saveValue)
-			}
-
-			saveValue = string(v)
+			continue
 		}
+
+		if saveValue == "\\" && string(v) == "\\" {
+			fmt.Fprintf(&b, "%s", saveValue)
+			duplicateSlash = true
+			saveValue = ""
+			continue
+		}
+
+		if saveValue != "" {
+			fmt.Fprintf(&b, "%s", saveValue)
+		}
+
+		saveValue = string(v)
 	}
 
 	if saveValue != "" {
