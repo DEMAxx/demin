e8a9ab3ee3e3f57dcfd5f668e82e691e8a597889
Stas Demin <demin.sv@cdek.ru>
2025-01-07 22:05:14 +0300

    Homework completed v1.0.0

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,7 +50,20 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(1)
+
+		for _, v := range [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9} {
+			c.Set(Key(strconv.Itoa(v)), v)
+		}
+
+		x, _ := c.Get(Key(strconv.Itoa(9)))
+
+		require.Equal(t, 9, x)
+
+		x, y := c.Get(Key(strconv.Itoa(1)))
+
+		require.Nil(t, x)
+		require.False(t, y)
 	})
 }
 
