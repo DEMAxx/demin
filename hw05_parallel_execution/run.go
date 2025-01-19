c9d4a18e61ac547e33dc0878efbd709c6bd54d35
Stas Demin <demin.sv@cdek.ru>
2025-01-19 10:55:05 +0300

    Homework completed v1.1.1 use WaitGroup

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -14,6 +15,8 @@ func Run(tasks []Task, n, m int) error {
 	isEnding := false
 	aj, i, ec := 0, 0, 0
 	results := make(chan Task, n)
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
 
 	for {
 		if ec > m {
@@ -26,9 +29,12 @@ func Run(tasks []Task, n, m int) error {
 
 		if i < count {
 			if aj < n {
+				wg.Add(1)
 				task := tasks[i]
 				i++
 				go func() {
+					defer wg.Done()
+
 					if err := task(); err != nil {
 						ec++
 					}
