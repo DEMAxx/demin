cdf73522e00726fddd2d3e75e34ef73695194019
Stas Demin <demin.sv@cdek.ru>
2025-01-21 01:07:14 +0300

    Homework completed v1.2.0 double loop

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -13,50 +13,63 @@ var mutex sync.Mutex
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
+	var e error
 	count := len(tasks)
 	isEnding := false
-	aj, i, ec := 0, 0, 0
-	results := make(chan Task, n)
+	stop := false
+	i, ec := 0, 0
+	chanJobs := make(chan Task, n)
+
+	jobsCount := 0
+	wg := sync.WaitGroup{}
+	defer func() {
+		if e == nil {
+			wg.Wait()
+		}
+	}()
+
+	for _, taskJob := range tasks {
+		wg.Add(1)
+		i++
+		go func() {
+			defer wg.Done()
+
+			if stop {
+				return
+			}
+			if err := taskJob(); err != nil {
+				ec++
+			}
+
+			chanJobs <- taskJob
+
+			if i >= n {
+				i = 0
+				isEnding = true
+			}
+		}()
+	}
 
 	for {
-		if ec > m {
-			return ErrErrorsLimitExceeded
-		}
+		if isEnding {
+			isEnding = false
 
-		if i >= count && aj == 0 {
-			break
-		}
-
-		if i < count {
-
-			if aj < n {
-				mutex.Lock()
-				task := tasks[i]
-				i++
-				mutex.Unlock()
-				go func() {
-					if err := task(); err != nil {
-						mutex.Lock()
-						ec++
-						mutex.Unlock()
-					}
-
-					results <- task
-				}()
-
-				mutex.Lock()
-				aj++
-				mutex.Unlock()
+			for _ = range chanJobs {
+				if ec >= m {
+					println("error")
+					e = ErrErrorsLimitExceeded
+					return ErrErrorsLimitExceeded
+				}
+				jobsCount++
+				if jobsCount >= count {
+					stop = true
+					return nil
+				}
 			}
-		} else {
-			isEnding = true
-		}
 
-		if aj >= n || isEnding {
-			<-results
-			mutex.Lock()
-			aj--
-			mutex.Unlock()
+			if stop {
+				break
+			}
 		}
 	}
 
