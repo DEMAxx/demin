c3a710c35a5363291f4f52c15d30a78bce9ad4ad
Stas Demin <demin.sv@cdek.ru>
2025-01-22 20:17:02 +0300

    Homework in progress v1.3.0

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,68 +10,60 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 var mutex sync.Mutex
+var mutex2 sync.Mutex
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	var e error
+	//var e error
 	count := len(tasks)
-	isEnding := false
-	stop := false
-	i, ec := 0, 0
-	chanJobs := make(chan Task, n)
-
+	//isEnding := false
+	errorCount := 0
 	jobsCount := 0
+	chanJobs := make(chan Task, count)
+	//done := make(chan int)
+
 	wg := sync.WaitGroup{}
+
 	defer func() {
-		if e == nil {
-			wg.Wait()
-		}
+		println("defer")
+		//if e == nil {
+		//	//wg.Wait()
+		//	close(chanJobs)
+		//}
 	}()
 
 	for _, taskJob := range tasks {
 		wg.Add(1)
-		i++
+
 		go func() {
 			defer wg.Done()
-
-			if stop {
-				return
-			}
-			if err := taskJob(); err != nil {
-				ec++
-			}
-
 			chanJobs <- taskJob
-
-			if i >= n {
-				i = 0
-				isEnding = true
-			}
 		}()
 	}
 
-	for {
-		if isEnding {
-			isEnding = false
+	wg.Wait()
 
-			for _ = range chanJobs {
-				if ec >= m {
-					println("error")
-					e = ErrErrorsLimitExceeded
-					return ErrErrorsLimitExceeded
-				}
-				jobsCount++
-				if jobsCount >= count {
-					stop = true
-					return nil
-				}
+	for {
+		for task := range chanJobs {
+			println("chanJob", jobsCount)
+
+			if errorCount >= m {
+				return ErrErrorsLimitExceeded
 			}
 
-			if stop {
-				break
+			jobsCount++
+
+			err := task()
+
+			if err != nil {
+				errorCount++
+			}
+
+			if jobsCount >= count {
+				return nil
 			}
 		}
-	}
 
-	return nil
+	}
+	//return nil
 }
