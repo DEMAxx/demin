978adcae19c156c8b071b63c9351ac09fddcf063
Stas Demin <demin.sv@cdek.ru>
2025-01-24 00:46:15 +0300

    Homework in progress v1.5.0

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,16 +10,26 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 func Run(tasks []Task, n, m int) error {
+	var e error
+
 	inputCh := make(chan Task)
 	errorCh := make(chan error)
 
+	sgnlCh := make(chan struct{})
+
 	wg := &sync.WaitGroup{}
+
+	j := 0
 
 	go func() {
 		defer close(inputCh)
 
 		for i := range tasks {
-			inputCh <- tasks[i]
+			select {
+			case inputCh <- tasks[i]:
+			case <-sgnlCh:
+				break
+			}
 		}
 	}()
 
@@ -27,32 +37,35 @@ func Run(tasks []Task, n, m int) error {
 		for i := 0; i < n; i++ {
 			wg.Add(1)
 
-			go worker(wg, inputCh, errorCh)
+			go worker(wg, inputCh, errorCh, sgnlCh)
 		}
 		wg.Wait()
 		close(errorCh)
 	}()
 
-	j := 0
-
-	for _ = range errorCh {
+	for range errorCh {
 		j++
 		if j == m {
-			return ErrErrorsLimitExceeded
+			close(sgnlCh)
+			e = ErrErrorsLimitExceeded
+			break
 		}
 	}
 
-	return nil
+	return e
 }
 
-func worker(wg *sync.WaitGroup, inCh <-chan Task, outCh chan<- error) {
+func worker(wg *sync.WaitGroup, inCh <-chan Task, outCh chan<- error, sgnlCh <-chan struct{}) {
 	defer wg.Done()
 
 	for task := range inCh {
 		err := task()
-
 		if err != nil {
-			outCh <- err
+			select {
+			case outCh <- err:
+			case <-sgnlCh:
+				return
+			}
 		}
 	}
 }
