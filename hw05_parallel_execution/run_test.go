5167e7be338a9ee5392f4ae71183217e49569a96
Stas Demin <demin.sv@cdek.ru>
2025-01-24 18:21:14 +0300

    Homework in completed v1.6.0

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,60 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("half tasks with error and no return error", func(t *testing.T) {
+		tasksCount := 40
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Second
+
+			if i%2 == 0 {
+				tasks = append(tasks, func() error {
+					time.Sleep(taskSleep)
+					return nil
+				})
+			} else {
+				err := fmt.Errorf("error from task %d", i)
+
+				tasks = append(tasks, func() error {
+					return err
+				})
+			}
+		}
+
+		workersCount := 10
+		maxErrorsCount := 23
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, nil), "actual err - %v", err)
+	})
+
+	t.Run("half tasks with error and return error", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Second
+
+			if i%2 == 0 {
+				tasks = append(tasks, func() error {
+					time.Sleep(taskSleep)
+					return nil
+				})
+			} else {
+				err := fmt.Errorf("error from task %d", i)
+
+				tasks = append(tasks, func() error {
+					return err
+				})
+			}
+		}
+
+		workersCount := 10
+		maxErrorsCount := 23
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	})
 }
