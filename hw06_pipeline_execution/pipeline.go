5d45f0cbb4550e39e1cc560efe642bfcdcf285be
Stas Demin <demin.sv@cdek.ru>
2025-02-01 19:00:05 +0300

    Homework in completed v0.1.0

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,10 @@
 package hw06pipelineexecution
 
+import (
+	"errors"
+	"sync"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +14,53 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var err error
+
+	if len(stages) == 0 {
+		return in
+	}
+
+	immediatelyStop := make(Bi)
+	defer close(immediatelyStop)
+
+	wg := &sync.WaitGroup{}
+
+	for _, stage := range stages {
+		wg.Add(1)
+		in, err = worker(in, done, stage, wg)
+		if err != nil {
+			println("error")
+			return in
+		}
+	}
+
+	wg.Wait()
+
+	return in
+}
+
+func worker(in In, done In, stage Stage, wg *sync.WaitGroup) (Out, error) {
+	var err error
+	out := make(Bi)
+
+	go func() {
+		wg.Done()
+
+		stageOut := stage(in)
+
+		defer close(out)
+
+		for i := range stageOut {
+			select {
+			case out <- i:
+				println("out <- i")
+			case <-done:
+				err = errors.New("done")
+				println("<-done")
+				return
+			}
+		}
+	}()
+
+	return out, err
 }
