aa0d111fdd197ae8020a4251c7b315b91c78859c
Stas Demin <demin.sv@cdek.ru>
2025-02-07 08:23:05 +0300

    Hw 8 started

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+)
+
+var ErrInvalidDir = errors.New("invalid directory")
+
+//var ErrInvalidFile = errors.New("invalid file")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +25,62 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	osDir, err := os.ReadDir(dir)
+	wg := &sync.WaitGroup{}
+	osEnv := make(Environment)
+
+	if err != nil {
+		return nil, ErrInvalidDir
+	}
+
+	for _, file := range osDir {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			var envValue EnvValue
+
+			osFile, err := os.Open(dir + "/" + file.Name())
+
+			if err != nil {
+				fmt.Println("Couldn't open file", err)
+				return
+			}
+
+			defer func(osFile *os.File) {
+				err := osFile.Close()
+				if err != nil {
+					fmt.Println("Couldn't close file")
+				}
+			}(osFile)
+
+			scanner := bufio.NewScanner(osFile)
+
+			for scanner.Scan() {
+				if len(strings.TrimSpace(scanner.Text())) > 0 {
+					envValue.Value = scanner.Text()
+					envValue.NeedRemove = false
+				} else {
+					envValue.Value = ""
+					envValue.NeedRemove = true
+				}
+
+				osEnv[file.Name()] = envValue
+				return
+			}
+
+			if err := scanner.Err(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	for key, env := range osEnv {
+		fmt.Println("key", key)
+		fmt.Println("env", env.Value)
+	}
+
+	return osEnv, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+
+	t.Run("wrong dir", func(t *testing.T) {
+		dir := "./test/env"
+
+		_, err := ReadDir(dir)
+
+		require.Truef(t, errors.Is(err, ErrInvalidDir), "actual err - %v", err)
+	})
+
+	t.Run("no empty values", func(t *testing.T) {
+		dir := "./testdata/env"
+
+		env, err := ReadDir(dir)
+
+		if err != nil {
+			println("error:", err.Error())
+		}
+
+		for key, val := range env {
+			fmt.Printf("test %s : %s\n", key, val.Value)
+
+		}
+
+		println(env)
+
+		//todo finish test to check is env have empty values
+	})
 }
