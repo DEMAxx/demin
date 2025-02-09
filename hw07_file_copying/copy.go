ec3a5509fe714991e9658a3f780a245716fd8eab
Stas Demin <demin.sv@cdek.ru>
2025-02-09 17:39:59 +0300

    Hw 7 finish with tests

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -3,10 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/cheggaaa/pb"
 	"io"
 	"os"
 	"time"
+
+	"github.com/cheggaaa/pb" //nolint
 )
 
 var (
@@ -19,7 +20,6 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	var sourceSize int64
 
 	s, err := os.Open(fromPath)
-
 	if err != nil {
 		fmt.Printf("Can't open %s: %v\n", fromPath, err)
 		return ErrUnsupportedFile
@@ -33,7 +33,6 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}(s)
 
 	sourceStat, err := s.Stat()
-
 	if err != nil {
 		fmt.Printf("Can't stat %s: %v\n", fromPath, err)
 		return ErrUnsupportedFile
@@ -53,12 +52,12 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 
 	source = s
 
-	// create dest
 	dest, err := os.Create(toPath)
 	if err != nil {
 		fmt.Printf("Can't create %s: %v\n", toPath, err)
 		return ErrUnsupportedFile
 	}
+
 	defer func(dest *os.File) {
 		err := dest.Close()
 		if err != nil {
@@ -85,53 +84,8 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	reader := bar.NewProxyReader(source)
 
 	io.Copy(dest, reader)
-	//if offset > 0 {
-	//	buf := make([]byte, limit)
-	//	_, err = CopyBufferAt(dest, reader, offset, buf)
-	//
-	//	if err != nil {
-	//		return err
-	//	}
-	//} else {
-	//	if limit > 0 {
-	//		io.CopyN(dest, reader, limit)
-	//	} else {
-	//		io.Copy(dest, reader)
-	//	}
-	//}
 
 	bar.Finish()
 
 	return nil
-}
-
-func CopyBufferAt(dest io.WriterAt, src io.Reader, off int64, buf []byte) (written int64, err error) {
-	for {
-		nr, er := src.Read(buf)
-		if nr > 0 {
-			nw, ew := dest.WriteAt(buf[0:nr], off+written)
-			if nw < 0 || nr < nw {
-				nw = 0
-				if ew == nil {
-					ew = errors.New("invalid write result")
-				}
-			}
-			written += int64(nw)
-			if ew != nil {
-				err = ew
-				break
-			}
-			if nr != nw {
-				err = io.ErrShortWrite
-				break
-			}
-		}
-		if er != nil {
-			if er != io.EOF {
-				err = er
-			}
-			break
-		}
-	}
-	return written, err
 }
