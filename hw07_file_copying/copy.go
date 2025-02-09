db028f36e8fdf8bbbab00381e58118cc7737f7ae
Stas Demin <demin.sv@cdek.ru>
2025-02-09 17:03:49 +0300

    Hw 7 finish without tests

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -3,12 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/cheggaaa/pb/v3"
+	"github.com/cheggaaa/pb"
 	"io"
-	"net/http"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -18,73 +15,123 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// check args
-	if len(os.Args) < 3 {
-		printUsage()
-		return ErrUnsupportedFile
-	}
-	sourceName, destName := os.Args[1], os.Args[2]
-
-	// check source
 	var source io.Reader
 	var sourceSize int64
-	if strings.HasPrefix(sourceName, "http://") {
-		// open as url
-		resp, err := http.Get(sourceName)
-		if err != nil {
-			fmt.Printf("Can't get %s: %v\n", sourceName, err)
-			return ErrUnsupportedFile
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			fmt.Printf("Server return non-200 status: %v\n", resp.Status)
-			return ErrUnsupportedFile
-		}
-		i, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
-		sourceSize = int64(i)
-		source = resp.Body
-	} else {
-		// open as file
-		s, err := os.Open(sourceName)
-		if err != nil {
-			fmt.Printf("Can't open %s: %v\n", sourceName, err)
-			return ErrUnsupportedFile
-		}
-		defer s.Close()
-		// get source size
-		sourceStat, err := s.Stat()
-		if err != nil {
-			fmt.Printf("Can't stat %s: %v\n", sourceName, err)
-			return ErrUnsupportedFile
-		}
-		sourceSize = sourceStat.Size()
-		source = s
-	}
 
-	// create dest
-	dest, err := os.Create(destName)
+	s, err := os.Open(fromPath)
+
 	if err != nil {
-		fmt.Printf("Can't create %s: %v\n", destName, err)
+		fmt.Printf("Can't open %s: %v\n", fromPath, err)
 		return ErrUnsupportedFile
 	}
-	defer dest.Close()
 
-	// create bar
-	//bar := pb.New(int(sourceSize)).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
-	bar := pb.New(int(sourceSize)).SetRefreshRate(time.Millisecond * 10)
-	//bar.ShowSpeed = true
+	defer func(s *os.File) {
+		err := s.Close()
+		if err != nil {
+			fmt.Printf("Can't close %s: %v\n", fromPath, err)
+		}
+	}(s)
+
+	sourceStat, err := s.Stat()
+
+	if err != nil {
+		fmt.Printf("Can't stat %s: %v\n", fromPath, err)
+		return ErrUnsupportedFile
+	}
+
+	sourceSize = sourceStat.Size()
+
+	if offset > 0 {
+		if offset > sourceSize {
+			return ErrOffsetExceedsFileSize
+		}
+		_, err = s.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	source = s
+
+	// create dest
+	dest, err := os.Create(toPath)
+	if err != nil {
+		fmt.Printf("Can't create %s: %v\n", toPath, err)
+		return ErrUnsupportedFile
+	}
+	defer func(dest *os.File) {
+		err := dest.Close()
+		if err != nil {
+			fmt.Printf("Can't close %s: %v\n", toPath, err)
+		}
+	}(dest)
+
+	copySize := int(limit)
+
+	if limit == 0 {
+		copySize = int(sourceSize)
+	}
+
+	bar := pb.New(copySize)
+	bar.SetUnits(pb.U_BYTES)
+	bar.SetRefreshRate(time.Millisecond * 10)
+	bar.ShowSpeed = true
 	bar.Start()
 
-	// create proxy reader
+	if limit > 0 {
+		source = io.LimitReader(source, limit)
+	}
+
 	reader := bar.NewProxyReader(source)
 
-	// and copy from reader
 	io.Copy(dest, reader)
+	//if offset > 0 {
+	//	buf := make([]byte, limit)
+	//	_, err = CopyBufferAt(dest, reader, offset, buf)
+	//
+	//	if err != nil {
+	//		return err
+	//	}
+	//} else {
+	//	if limit > 0 {
+	//		io.CopyN(dest, reader, limit)
+	//	} else {
+	//		io.Copy(dest, reader)
+	//	}
+	//}
+
 	bar.Finish()
 
 	return nil
 }
 
-func printUsage() {
-	fmt.Println("copy [source file or url] [dest file]")
+func CopyBufferAt(dest io.WriterAt, src io.Reader, off int64, buf []byte) (written int64, err error) {
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dest.WriteAt(buf[0:nr], off+written)
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = errors.New("invalid write result")
+				}
+			}
+			written += int64(nw)
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
 }
