261676179fefd713862d3fe819f066699957d167
Stanislav Demin <demin.sv@cdek.ru>
2025-03-03 20:32:32 +0300

    Hw 11 in progress

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,122 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/spf13/pflag"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+func readRoutine(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	scanner := bufio.NewScanner(conn)
+	defer log.Printf("Finished readRoutine")
+
+OUTER:
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("readRoutine is done")
+			return
+		default:
+			if !scanner.Scan() {
+				log.Printf("CANNOT SCAN")
+				break OUTER
+			}
+			text := scanner.Text()
+			log.Printf("From server: %s", text)
+		}
+	}
+}
+
+func writeRoutine(ctx context.Context, conn net.Conn, wg *sync.WaitGroup, stdin chan string) {
+	defer wg.Done()
+	defer log.Printf("Finished writeRoutine")
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case str := <-stdin:
+			fmt.Printf("To server %v\n", str)
+
+			conn.Write([]byte(fmt.Sprintf("%s\n", str)))
+		}
+
+	}
+}
+
+func stdinScan() chan string {
+	out := make(chan string)
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			out <- scanner.Text()
+		}
+		if scanner.Err() != nil {
+			close(out)
+		}
+	}()
+	return out
+}
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var err error
+	var intervalFlag time.Duration
+
+	pflag.DurationVarP(&intervalFlag, "timeout", "t", time.Second*10, "timeout of each event")
+
+	pflag.Lookup("timeout").NoOptDefVal = "noconfig.json"
+
+	pflag.Parse()
+
+	dialer := &net.Dialer{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), intervalFlag)
+	defer cancel()
+
+	//ctxNotify, stop := signal.NotifyContext(ctx, syscall.SIGTERM)
+	//defer stop()
+
+	conn, err := dialer.DialContext(ctx, "tcp", net.JoinHostPort("localhost", "4242"))
+
+	if err != nil {
+		log.Fatalf("Can not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	//client := NewTelnetClient(
+	//	net.JoinHostPort("localhost", "4242"),
+	//	intervalFlag,
+	//	conn,
+	//	conn,
+	//)
+	//
+	//_ = client
+
+	//conn.Write([]byte(fmt.Sprintf("%s\n", "You are connected")))
+
+	stdin := stdinScan()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		readRoutine(ctx, conn, wg)
+		cancel()
+	}()
+
+	wg.Add(1)
+	go func() {
+		writeRoutine(ctx, conn, wg, stdin)
+	}()
+
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -13,9 +17,42 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	out.Write([]byte(fmt.Sprintf("%s\n", "You are connected")))
+	fmt.Printf("NewTelnetClient: You are connected")
+
+	stdin := stdinScan()
+
+	for {
+		select {
+		case str := <-stdin:
+			log.Println("STDIN", str)
+		case <-time.After(timeout - time.Second):
+			log.Println("got timeout", timeout)
+			in.Close()
+			return nil
+		}
+	}
 }
 
 // Place your code here.
 // P.S. Author's solution takes no more than 50 lines.
+func handleConnection(conn net.Conn) {
+	fmt.Fprintf(conn, "Welcome to %s, friend from %s\n", conn.LocalAddr(), conn.RemoteAddr())
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		log.Printf("RECEIVED: %s", text)
+		if text == "quit" || text == "exit" {
+			break
+		}
+
+		conn.Write([]byte(fmt.Sprintf("I have received '%s'\n", text)))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error happend on connection with %s: %v", conn.RemoteAddr(), err)
+	}
+
+	log.Printf("Closing connection with %s", conn.RemoteAddr())
+}
